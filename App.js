10a0241192bd0e73070b5ae7692cb6ed30f00cd5
saihajvir <saihajvir@hotmail.com>
2021-10-11 11:17:47 -0700

    added magnus-ui framework

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, AppRegistry } from 'react-native';
 import styled from 'styled-components';
 import LottieView from 'lottie-react-native';
-import ButtonUI from './comps/MainButton/index'
+import ButtonUI from './comps/MainButton/index';
+import { ThemeProvider, Text, Div, Button, Icon } from 'react-native-magnus';
 
+
+const ffTheme = {
+  colors: {
+    periwinkle: "#92A8F8"
+  }
+}
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-      <ButtonUI/>
-    </View>
+    <ThemeProvider theme={ffTheme}>
+      <Div style={styles.container}>
+        <Text fontSize='6xl' fontWeight='600' color='periwinkle'>Welcome to Frefurnish.</Text>
+      </Div>
+    </ThemeProvider>
   );
 }
 
