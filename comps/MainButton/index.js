2b60c1976ace4d1351f6331dae2efdf0ada413d2
saihajvir <saihajvir@hotmail.com>
2021-10-17 18:11:55 -0700

    remade button with magnus-ui, added margin 16px to app.js

--- comps/MainButton/index.js
+++ comps/MainButton/index.js
@@ -1,31 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon } from 'react-native-magnus'
 import styled from 'styled-components';
 import LottieView from 'lottie-react-native';
 
-const Button = styled.View`
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 396px;
-    height: 50px;
-    border-radius: 20px;
-    background-color: #92A8F8;
-
-`
-const ButtonText = styled.Text`
-
-    color: white;
-    font-weight: 500;
-`
-
-export default function ButtonUI() 
+export default function MainButton({
+    buttonText = "default text",
+    iconName = "ios-arrow-forward-circle",
+    iconFamily = "Ionicons"
+}) 
 {
     return (
-        <Button onClick={()=>{}}>
-            <ButtonText>default</ButtonText>
+        <Button
+            bg='periwinkle'
+            block
+            color='white'
+            rounded={20}
+            h={50}
+            fontSize={18}
+            fontWeight='500'
+            suffix={
+                <Icon
+                 name={iconName} 
+                 fontFamily={iconFamily}
+                 position='absolute' 
+                 right={10} 
+                 color='white' 
+                 fontSize={24}
+                />
+            }
+        >
+            {buttonText}
         </Button>
     );
 }
