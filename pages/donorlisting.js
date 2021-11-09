a1b90db670e94a0c5c54f2ddfebc6dd31ade61a2
gurvirdhaliwal99 <gurvir_99@hotmail.com>
2021-11-08 20:40:10 -0800

    fixed inconsistencies

--- pages/donorlisting.js
+++ pages/donorlisting.js
@@ -1,16 +1,19 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import styled from "styled-components";
-import { ScrollView, View, TouchableOpacity, ImageBackground} from "react-native";
+import { ScrollView, View } from "react-native";
 
-import { ThemeProvider, Text, Div, Button, Icon, ScrollDiv } from 'react-native-magnus';
+import { ThemeProvider, Text, Div, Button, Icon, ScrollDiv, Input } from 'react-native-magnus';
 
 import MainButton from '../comps/MainButton/index';
 import BottomNav from '../comps/BottomNavBar';
 import ItemIcon from "../comps/ItemIcon";
 import Header1 from "../comps/header";
 import SmallPost from "../comps/SmallPost";
-
+import BackButton from "../comps/backbutton";
+import HalfButton from "../comps/halfbutton";
+import BigPost from "../comps/BigPost";
+import MainPost from "../comps/MainPost";
 
 
 const ffTheme = {
@@ -22,22 +25,20 @@ const ffTheme = {
     }
   }
 
-const Requests = styled.View`
-    padding: 10px 16px 0 16px;
-    flex:0.5;
+const Wrapper = styled.View`
+    padding: 0 10px 0 10px;
+    justify-content: space-between;
+    align-items: center;
     background-color: #FFFFFF;
 `
-const Wrapper = styled.View`
-    padding: 0 16px 0 16px;
-    flex-direction: row;
+const ListingText = styled.View`
+    flex:1;
+    padding: 0 10px 0 10px;
     background-color: #FFFFFF;
 `
 const Container = styled.View`
-    flex:0.5;
     flex-direction: row;
     padding: 0 10px 0 10px;
-    justify-content: center;
-    align-items: center;
     background-color: #FFFFFF;
 `
 const Navbar = styled.View`
@@ -45,76 +46,31 @@ const Navbar = styled.View`
     align-items: center;
     background-color: #FFFFFF;
 `
-const RequestBox = styled.View`
-    background-color: #92A8F8;
-`
 
-export default function donorHome({route, navigation})
+export default function donorListing({route, navigation})
 {
     return (
         <ThemeProvider theme={ffTheme}>
-            <Requests>
-                <Text fontWeight="bold" fontSize={36}>Requests</Text>
-                <RequestBox>
-                    
-                </RequestBox>
-            </Requests>
-            
-            <Wrapper>
-            <MainButton buttonText={'See All Requests'} bg="periwinkle" textColor='white'/>
-            </Wrapper>
+        <Container>
+            <MainPost />
+        </Container>
 
-            <Wrapper>
-            <Text fontWeight="bold" fontSize={36}>Your Listings</Text>
-            </Wrapper>
-            
-            <Container>
-                <ScrollView horizontal={true}>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                </ScrollView>
-            </Container>
+        <ListingText>
+            <Text fontWeight="bold" fontSize={24}>Condition</Text>
+            <Text fontSize={14}>Used</Text>
+            <Text fontWeight="bold"  fontSize={24}>Description</Text>
+            <Text fontSize={14}>Just an old office chair we don't use anymore.</Text>
+            <Text fontWeight="bold"  fontSize={24}>Feedback</Text>
+        </ListingText>
 
+        <Wrapper>
+            <MainButton buttonText="Edit Listing" onPress={() => {navigation.navigate("")}}/> 
+        </Wrapper>
 
-            <Wrapper>
-            <Text fontWeight="bold" fontSize={36}>Past Donations</Text>
-            </Wrapper>
-            <Container>
-                <ScrollView horizontal={true}>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => {navigation.navigate("donorListing")}}>
-                        <SmallPost/>
-                    </TouchableOpacity>
-                </ScrollView>
-            </Container>
-
-            <Navbar>
-                <BottomNav />
-            </Navbar>
+        <Navbar>
+            <BottomNav/>
+        </Navbar>
+         
         </ThemeProvider>
     )
 }
