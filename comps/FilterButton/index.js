00ad1d78b68ee1f83083a86f752aa0c1cc557e7d
joshbreyes <jreyes46@my.bcit.ca>
2021-11-22 16:17:29 -0800

    filterbutton cant click idk why, reg button works

--- comps/FilterButton/index.js
+++ comps/FilterButton/index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 import { Div, Text, Image, Button, Icon } from "react-native-magnus";
 import styled from 'styled-components';
 import LottieView from 'lottie-react-native';
@@ -15,33 +15,33 @@ export default function FilterButton({
 
 {
     return(
-        <Div row flexWrap="wrap">
-            <Button
-                w = {buttonWidth}
-                h = {buttonHeight}
-                ml = {marginLeft}
-                px = "xl"
-                py = "lg"
-                bg = "white"
-                borderWidth = {1}
-                rounded = {10}
-                bg = "#92A8F8" 
-                borderColor = "#92A8F8"
-                color = "white"
-                underlayColor = "92A8F8"
-            >
-                <Icon 
-                name = "filter" 
-                color = "white"
-                mr = "10%"
-                />
-                <Text
-                color = "white"
+        <Button
+        w = {buttonWidth}
+        h = {buttonHeight}
+        ml = {marginLeft}
+        mt = "5%"
+        px = "xl"
+        py = "lg"
+        bg = "white"
+        borderWidth = {1}
+        rounded = {10}
+        bg = "#92A8F8" 
+        borderColor = "#92A8F8"
+        color = "white"
+        underlayColor = "92A8F8"
+        row flexWrap="wrap"
+    >
+        <Icon 
+        name = "filter" 
+        color = "white"
+        mr = "10%"
+        />
+        <Text
+        color = "white"
 
-                >
-                    Filter Items
-                </Text>
-            </Button>
-        </Div>
+        >
+            Filter Items
+        </Text>
+    </Button>
     );
 }
